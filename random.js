474ad335def3b1d25fa9433c435c3cebced7a3f2
Maximilian Dollinger <70093586+maxdollinger@users.noreply.github.com>
2020-11-27 16:43:58 +0100

    Update random.js

--- random.js
+++ random.js
@@ -1,60 +1,52 @@
+function generate(options) {
+  const { type, length, upperCase, special, charSet } = { ...options };
+  const num = "0123456789";
+  const chars = "abcdefghijklmnopqrstuvwxyz";
+  const spe = `!"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`;
 
-class Random {
+  let str;
 
-  constructor() {
-    this.num = '0123456789';
-    this.charsLC = 'abcdefghijklmnopqrstuvwxyz';
-    this.charsUC = this.charsLC.toUpperCase();
-    this.special = '!@#$%&*_-=+;:,<>.|'
+  switch (type) {
+    default:
+    case "numeric":
+      str = num;
+      break;
+    case "alphabetic":
+      str = chars;
+      break;
+    case "alphanumeric":
+      str = chars + num;
+      break;
+    case "hex":
+      str = num + "abcdef";
+      break;
+    case "binary":
+      str = "01";
+      break;
+    case "special":
+      str = num + chars + spe;
+      break;
   }
 
-  random(length, chars) {
-    let str = [];
-
-    for(let i = 1; i <= length; i++) {
-    str.push(chars[Math.floor(Math.random() * chars.length)]);
-    }
-
-    return str.join('');
+  if (upperCase === "with") {
+    str = str.replace(/[a-z]+/g, (el) => el + el.toUpperCase());
   }
+  if (upperCase === "only") str = str.toUpperCase();
+  if (charSet) str = charSet;
+  if (special) str += special;
 
-  numStr(length) {
-    return this.random(length, this.num)
-  }
-
-  hexStr(length) {
-    return this.random(length, (this.num+'abcdef'));
-  }
-
-  binStr(length) {
-    return this.random(length, '01');
-  }
-
-  charStr(length, option) {
-    let charSet = this.charsLC;
-    if(option === 1) charSet = this.charsLC + this.charsUC;
-    if(option === 2) charSet = this.charsUC;
-
-    return this.random(length, charSet)
-  }
-
-  charNumStr(length, option) {
-    let charSet = this.num + this.charsLC;
-    if(option === 1) charSet += this.charsUC;
-    if(option === 2) charSet = this. num + this.charsUC;
-
-    return this.random(length, charSet)
-  }
-
-  charNumSpeStr(length, special, option) {
-    special = special ? special : '!@#$%&*_-=+;:,<>.|';
-    let charSet = this.num + special +  this.charsLC;
-    if(option === 1) charSet += this.charsUC;
-    if(option === 2) charSet =  this.num + special +  this.charsLC;
-
-    return this.random(length, charSet)
-  }
+  return random(length, str);
 }
 
-const instance = new Random();
-module.exports = instance;
+function random(length, chars) {
+  length = length ? length : 32;
+  let str = [];
+
+  for (let i = 1; i <= length; i++) {
+    str.push(chars[Math.floor(Math.random() * chars.length)]);
+  }
+
+  return str.join("");
+}
+
+module.exports = generate;
